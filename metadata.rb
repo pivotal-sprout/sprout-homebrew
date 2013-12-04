75bc41d8fd228df6403e98c3a8c109bdb8119ea8
Abhi Hiremagalur <abhi@pivotallabs.com>
2013-12-04 08:29:05 +0530

    Make metadata.rb consistent with other sprout cookbooks

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             "sprout-homebrew"
-maintainer       "Brian Cunnie"
-maintainer_email "brian.cunnie@gmail.com"
-license          "All rights reserved"
-description      "Meta recipes to install homebrew formulae and casks"
+maintainer       "Pivotal"
+maintainer_email "accounts@pivotallabs.com"
+license          "MIT"
+description      "Installs homebrew formulae and casks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "homebrew"
