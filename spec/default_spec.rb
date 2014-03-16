944ced41652665a0fba02ba7c5fbb45e57f63692
Alexander Wenzowski <alexander@wenzowski.com>
2014-03-16 00:39:17 -0400

    swap `pstree` for `tor` and test daemon support

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,13 +2,20 @@ require 'spec_helper'
 
 describe 'sprout-homebrew' do
   before :all do
+    system 'brew services stop tor'
+    system 'brew uninstall tor'
+    system 'brew cask uninstall google-chrome'
     system 'soloist' or fail 'soloist failed'
   end
-  
-  it 'installs pstree as an example formula' do
-    expect(system('which pstree')).to be_true
+
+  it 'installs tor as an example formula' do
+    expect(system('which tor')).to be_true
   end
-  
+
+  it 'loads the tor agent as an example daemon' do
+    expect(system('pgrep -x tor')).to be_true
+  end
+
   it 'installs Google Chrome as an example cask' do
     expect(File.exist?('/Applications/Google Chrome.app')).to be_true
   end
