7fac1b808cb164d10d359800f103c0394f1e825b
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-08 11:53:13 -0700

    Be less strict about homebrew version
    So sprout-osx-apps can use a more recent version of the homebrew cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      "Installs homebrew formulae and casks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends          "homebrew", "~> 1.5.4"
+depends          "homebrew", ">= 1.5.4"
