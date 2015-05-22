69aab9a5a4b07535cc903e1db97a8099c4f1d3f6
Jonathan Barnes <jbarnes@pivotal.io>
2015-05-21 17:19:18 -0700

    Adds bash_it template to install casks in /Applications
    Also bumps the cookbook minor version for this new feature.
    Signed-off-by: Hua Zhang <zhuadl@cn.ibm.com>

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "sprout-maintainers@pivotallabs.com"
 license          "MIT"
 description      "Installs homebrew formulae and casks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends          "homebrew", ">= 1.5.4"
