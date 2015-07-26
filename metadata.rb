44627dcee9e633a5c409f9b355f62f336dabff72
Dan Wendorf <dwendorf@pivotal.io>
2015-07-26 14:36:21 -0700

    Depend on sprout-base to get node['sprout']['user'] set

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 supports         'mac_os_x'
 depends          'homebrew', '>= 1.5.4'
+depends          'sprout-base'
